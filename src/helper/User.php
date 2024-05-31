5a2dac5ab7c0d5b4178c03f48ad98b4bd3ae3d27
王龙起 <13518146310@100fen.cn>
2024-05-31 14:46:33 +0800

    feat: 紫微命盘

--- src/helper/User.php
+++ src/helper/User.php
@@ -11,9 +11,33 @@ class User
      */
     protected static $user = null;
 
-    public static function init(array $data): UserModel
-    {
-        self::$user = new UserModel($data);
+    /**
+     * 初始化
+     *
+     * @param string $date  日期时间，格式：Y-m-d H:i:s
+     * @param int $dateType 日期类型，1=阳历，2=阴历
+     * @param bool $isLeapMonth 是否是闰月
+     * @param int $sex  性别，0=女，1=男
+     * @param string $city
+     * @return UserModel
+     *
+     * @author wlq
+     * @since 1.0 2024-05-31
+     */
+    public static function init(
+        string $date,
+        int $dateType = 1,
+        bool $isLeapMonth = false,
+        int $sex = 0,
+        string $city = ''
+    ): UserModel {
+        self::$user = new UserModel([
+            'gregorianBirthday' => $dateType == 1 ? $date : '',//出生日期-阳历
+            'lunarBirthday' => $dateType == 2 ? $date : '',//出生日期-阴历
+            'isLeapMonth' => $isLeapMonth,//是否为农历闰月，默认否
+            'sex' => $sex,
+            'city' => $city,
+        ]);
         return self::$user;
     }
 
